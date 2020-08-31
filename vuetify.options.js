3eb1a7463c4a6bf877ae08dcd93ecfb9db5b27d0
Ori Alvarez <ori.alvarez@commithub.com>
2020-08-31 13:20:39 -0400

    used darkmode instead

--- vuetify.options.js
+++ vuetify.options.js
@@ -4,16 +4,6 @@ export default {
       customProperties: true,
     },
     themes: {
-      light: {
-        // Colors are taken from the design system for CommitHub
-        primary: "#3282B8",
-        secondary: "#424242",
-        accent: "#21C6CE",
-        error: "#C72C41", // danger
-        info: "#2196F3",
-        success: "#29C7AC",
-        warning: "#FFBF00",
-      },
       dark: {
         primary: "#3282B8",
         secondary: "#424242",
